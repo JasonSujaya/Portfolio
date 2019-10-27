78e7131090c970a62ca286be30c36335a9f46db9
jasonsujaya <sujaya.jason@gmail.com>
2019-10-27 11:25:19 +0100

    Name Changes

--- ReactNative/food/src/api/yelp.js
+++ ReactNative/food/src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorizations:
+    Authorization:
       "Bearer FETF52hmD4eFZ8EE4nIU8cpOsKU_muY6xjQvOeW7pz8Ll4Jc9nxMnwqjE9ESO3KWQyRXG0edjT5AaChpEOJ57x73Zs-iD70-cd5YPoZ_wcP-S1qEM2xCJh0JoJOwXXYx      "
   }
 });
